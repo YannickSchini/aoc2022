1f584aaedfe9df63d78f351463804fa81250589b
Yannick Schini <yannick.schini@backmarket.com>
2022-12-02 10:09:32 +0100

    feat: day02 part 2

--- day02/calculate_score.py
+++ day02/calculate_score.py
@@ -20,6 +20,16 @@ def convert_raw_to_moves(raw: str) -> Moves:
     else:
         Exception("UnknownMoveException")
 
+def convert_raw_to_results(raw: str) -> Result:
+    if raw == "X":
+        return Result.LOSS
+    elif raw == "Y":
+        return Result.DRAW
+    elif raw == "Z":
+        return Result.WIN
+    else:
+        Exception("UnknownResultException")
+
 def get_result_for_player_2(player_1_move: Moves, player_2_move: Moves) -> Result:
     if player_1_move == player_2_move:
         return Result.DRAW
@@ -28,18 +38,46 @@ def get_result_for_player_2(player_1_move: Moves, player_2_move: Moves) -> Resul
     else:
         return Result.WIN
 
-def player_2_score(player_1_raw_move: str, player_2_raw_move: str) -> int:
+def get_move_for_player_2(player_1_move: Moves, target_result: Result) -> Result:
+    if target_result == Result.DRAW:
+        return player_1_move
+    elif player_1_move == Moves.ROCK:
+        if target_result == Result.WIN:
+            return Moves.PAPER
+        else:
+            return Moves.SCISSORS
+    elif player_1_move == Moves.PAPER:
+        if target_result == Result.WIN:
+            return Moves.SCISSORS
+        else:
+            return Moves.ROCK
+    elif player_1_move == Moves.SCISSORS:
+        if target_result == Result.WIN:
+            return Moves.ROCK
+        else:
+            return Moves.PAPER
+    else:
+        Exception("WTF is happening?!")
+
+def player_2_score_part_1(player_1_raw_move: str, player_2_raw_move: str) -> int:
     player_1_move = convert_raw_to_moves(player_1_raw_move)
     player_2_move = convert_raw_to_moves(player_2_raw_move)
     result = get_result_for_player_2(player_1_move, player_2_move)
     return player_2_move.value + result.value
 
+def player_2_score_part_2(player_1_raw_move: str, expected_result_raw: str) -> int:
+    player_1_move = convert_raw_to_moves(player_1_raw_move)
+    expected_result = convert_raw_to_results(expected_result_raw)
+    player_2_move = get_move_for_player_2(player_1_move, expected_result)
+    return player_2_move.value + expected_result.value
+
 
 if __name__ == "__main__":
     total_score = 0
-    file = "train.txt" # Score should be 15
-    # file = "input.txt"
+    # file = "train.txt" # Score should be 15 for part 1, 12 for part 2
+    file = "input.txt"
     with open(file) as f:
         for line in f.readlines():
-            total_score += player_2_score(*line.strip().split(" "))
+            # total_score += player_2_score_part_1(*line.strip().split(" "))
+            total_score += player_2_score_part_2(*line.strip().split(" "))
     print(total_score)
