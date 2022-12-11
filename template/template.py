8c3f7a4ce18cbdada0a1f7e47f0f59234a4dac06
Yannick Schini <yannick.schini@backmarket.com>
2022-12-11 10:11:36 +0100

    fix: Correct whitespace in template program

--- template/template.py
+++ template/template.py
@@ -1,7 +1,7 @@
 from argparse import ArgumentParser, FileType
 
 if __name__ == "__main__":
-    parser = ArgumentParser(prog = "TODO", description = "TODO")
+    parser = ArgumentParser(prog="TODO", description="TODO")
     parser.add_argument("filename", type=FileType("r"))
     args = parser.parse_args()
 
